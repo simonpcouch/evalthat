84c85aa43e68dd4f913c47ee11d46f63ba0d0970
simonpcouch <simonpatrickcouch@gmail.com>
2024-12-03 15:55:48 -0600

    add fn to read results

---
+++ R/results.R
@@ -0,0 +1,22 @@
+#' Read eval results
+#'
+#' @param dir Optional. A single string specifying a subdirectory of
+#' `tests/evalthat/_results`. If `NULL`, will read each subdirectory and
+#' row-bind the results.
+#'
+#' @returns
+#' A tibble containing the combined results from result files in the
+#' specified directory.
+#'
+#' @export
+results_read <- function(dir = NULL) {
+  dir <- paste0("tests/evalthat/_results", dir, collapse = "/")
+  result_files <- list.files(dir, full.names = TRUE, recursive = TRUE)
+
+  res <- tibble::tibble()
+  for (file in result_files) {
+    res <- vctrs::vec_rbind(res, qs::qread(file))
+  }
+
+  res
+}
